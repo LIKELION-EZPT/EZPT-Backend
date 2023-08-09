1f2d52f42f7f57ca75c86e8a98f580df35247670
niceyoo0221 <niceyoo0221@knu.ac.kr>
2023-08-09 21:30:30 +0900

    Initial Commit

---
+++ src/main/java/dev/likelion/ezpt/EZPT/EzptApplication.java
@@ -0,0 +1,13 @@
+package dev.likelion.ezpt.EZPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/likelion/ezpt/EZPT/EzptApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.likelion.ezpt.EZPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
